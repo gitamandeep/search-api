84c08c5ae40883a4f7c68f043c0216f1f0470b67
aman deep <patnacityamandeep@gmail.com>
2022-07-21 12:56:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+require('./config');
+const product = require('./product');
+const app = express();
+app.use(express.json());
+
+app.get("/search/:key",async (req,res)=>{
+    console.log(req.params.key)
+    let data = await product.find(
+        {
+            "$or":[
+                {"name":{$regex:req.params.key}},
+                {"brand":{$regex:req.params.key}}
+            ]
+        }
+    )
+})
